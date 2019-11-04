f8ebc2b5015774af77b64a1690d974d79584668a
unknown <aniketbrahmecha@gmail.com>
2019-11-04 10:37:45 +0530

    Music App Part1

---
+++ constants/Colors.js
@@ -0,0 +1,27 @@
+const tintColor = '#2f95dc';
+
+export default {
+  tintColor,
+  tabIconDefault: '#ccc',
+  tabIconSelected: tintColor,
+  tabBar: '#fefefe',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+
+  //Our colors
+  primaryColor:'#191B2F',
+  accentColor:'#9458EA',
+  placeholderColor:'#989CC1',
+  accentGradientStart:'#8250AF',
+  accentGradientEnd:'#3A2496',
+  primaryGradientStart:'#1E2542',
+  primaryGradientEnd:'#16162C',
+  headingColor:'#FFF',
+  greyColor:'#aaa',
+  blueColor: '#363b66',
+  lightAccentColor: '#8567CB'
+};
---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import {responsiveHeight,responsiveFontSize} from "react-native-responsive-dimensions";
+import {MaterialIcons} from "@expo/vector-icons";
+
+import TabBarIcon from '../components/TabBarIcon';
+import SongsScreen from '../screens/SongsScreen';
+import SearchScreen from '../screens/SearchScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+import Colors from "../constants/Colors";
+import  NowPlayingScreen from "../screens/NowPlayingScreen";
+
+
+const config = Platform.select({
+  web: { headerMode: 'screen' },
+  default: {},
+});
+
+const HomeStack = createStackNavigator(
+  {
+    Home: SongsScreen,
+      NowPlaying: NowPlayingScreen
+  },
+  config
+);
+
+HomeStack.navigationOptions = {
+  tabBarIcon: ({ focused }) => (
+   <MaterialIcons name ={'music-note'} size={responsiveFontSize(4)} color={Colors.accentColor}></MaterialIcons>
+  ),
+};
+
+HomeStack.path = '';
+
+const LinksStack = createStackNavigator(
+  {
+    Links: SearchScreen,
+  },
+  config
+);
+
+LinksStack.navigationOptions = {
+  tabBarIcon: ({ focused }) => (
+      <MaterialIcons name ={'search'} size={responsiveFontSize(4)} color={Colors.accentColor}></MaterialIcons>
+  ),
+};
+
+LinksStack.path = '';
+
+const SettingsStack = createStackNavigator(
+  {
+    Settings: SettingsScreen,
+  },
+  config
+);
+
+SettingsStack.navigationOptions = {
+  tabBarIcon: ({ focused }) => (
+      <MaterialIcons name ={'person'} size={responsiveFontSize(4)} color={Colors.accentColor}></MaterialIcons>
+  ),
+};
+
+SettingsStack.path = '';
+
+const tabNavigator = createBottomTabNavigator({
+  HomeStack,
+  LinksStack,
+  SettingsStack,
+},{
+    tabBarOptions:{
+        showLabel:false,
+        style:{
+            backgroundColor :Colors.primaryColor,
+            height:responsiveHeight(10),
+        }
+    }
+});
+
+tabNavigator.path = '';
+
+export default tabNavigator;
